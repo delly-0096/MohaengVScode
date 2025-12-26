899a7e9d5f23276f046475366e0a5937f3ec72e7
donggeun <dongkun8130@gmail.com>
2025-12-26 10:16:02 +0900

    수정해봤습니다. 같은사람

---
+++ src/ddtest.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ddtest() {
+    return (
+        <div>
+
+            ddtest
+
+            <h3>테에에에에스트</h3>
+        </div>
+
+    )
+}
+
+export default ddtest
