3f85fc74252c3bd4b2497c1556eed01d7574855a
ksh <135tjgus@gmail.com>
2026-01-28 18:22:41 +0900

    [react] 김서현 / 서비스 통계 구현 완료

--- src/pages/contents/ProductInquiries.jsx
+++ src/pages/contents/ProductInquiries.jsx
@@ -8,7 +8,9 @@ const categories = [
   { value: 'booking', label: '예약/일정' },
   { value: 'price', label: '가격/결제' },
   { value: 'cancel', label: '취소/환불' },
-  { value: 'other', label: '기타' }
+  { value: 'other', label: '기타 문의' },
+  { value: 'room', label: '객실 문의' },
+  { value: 'facility', label: '시설/서비스' }
 ];
 
 // 상태 (SECRET_YN: Y=비밀글, N=공개글, H=숨김)
@@ -422,7 +424,7 @@ function ProductInquiries() {
         <table className="data-table" style={{ width: '100%' }}>
           <thead>
             <tr>
-              <th style={{ width: '100px' }}>카테고리</th>
+              <th style={{ width: '110px' }}>카테고리</th>
               <th style={{ width: '250px' }}>상품명</th>
               <th>문의 내용</th>
               <th style={{ width: '110px' }}>문의자</th>
